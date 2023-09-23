1e2a5f632fecb89cb11fe69df1b462dedbb98b5f
Jayanth Parthsarathy <jparthaa@gmail.com>
2023-09-23 19:25:34 +0530

    Add ayu colors

---
+++ ayu.h
@@ -0,0 +1,7 @@
+static const char *colors[SchemeLast][2] = {
+    /*     fg         bg       */
+    [SchemeNorm] = { "#e5e1cf", "#0e1419" }, // Background: #0e1419, Foreground: #e5e1cf
+    [SchemeSel] = { "#0e1419", "#b8cc52" }, // Background: #b8cc52, Foreground: #0e1419
+    [SchemeOut] = { "#000000", "#b8cc52" }, // Background: #b8cc52, Foreground: #000000
+};
+
